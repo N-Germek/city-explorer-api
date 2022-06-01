77c40d817a7bf6d401ced4e5dd343b26aea42438
Natalija Germek <n.germek@icloud.com>
2022-06-01 10:17:20 -0700

    completed front End Weather component data

--- server.js
+++ server.js
@@ -6,8 +6,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const weather = require('./data/weather.json');
 const { response } = require('express');
-const { type } = require('express/lib/response');
 
 //will be using express variable to instantiate a new instance of the Express server
 const app = express();
@@ -19,31 +19,35 @@ app.use(cors());
 const PORT = process.env.PORT || 3002;
 
 //testing if PORT server works.
+// GET = READ (get me some data, read me some data)
 app.get('/', (request, response) => {
     response.send('testing');
 });
 
-// creating weather endpoint, route and searching with req and res paramiters.
-app.get('/weather', (req, res) => {
-    const type = req.query.type;
-    const forcastResults = new Forcast(type);
-    console.log("Forcast Results: ", forcastResults);
-    // console.log('type of list requested: ', type);
-    res.send('test from weather endpoint.');
-});
-
-//weather variable is bananas
 class Forcast {
     static weather = require('./data/weather.json');
-
-    constructor(type) {
-        this.type = Forcast.weather.find(listObj => listObj.lon && listObj.lat && listObj.city_name === type);
-        this.array = this.type.data.map(weatherObj => ({
-            "date": weatherObj.datetime
+    
+    constructor(day) {
+        this.info = Forcast.weather.find(listObj => listObj.city_name.toLowerCase() === day.toLowerCase());
+        this.array = this.info.data.map(weatherObj => ({
+            "date": weatherObj.datetime,
+            "description": weatherObj.weather.description
         }))
         console.log(this.array);
     }
 };
+// creating weather endpoint, route and searching with req and res paramiters.
+app.get('/weather', (req, res) => {
+
+    const type = req.query.type;
+    const forcastResults = new Forcast(type);
+    console.log("Forcast Results: ", forcastResults);
+    console.log('type of list requested: ', type);
+    res.send(forcastResults.array);
+});
+
+// weather variable is bananas
+console.log(weather);
 
 //telling app(express server) where (which port) to run on
 // listening for PORT argument then callback function (this is the anonymous function)
---
+++ weather.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Weather.js';
+
+Class Weather extends React.Component {
+    constructor(props) {
+        super(props);
+    this.state = {
+        forcaseArr = []
+    }    
+}
+render() {
+    return (
+        <div className="Weather">
+<button onClick={this.forcaseArr}
+
+
+
+        </div>
+    )
+}
+
+}
